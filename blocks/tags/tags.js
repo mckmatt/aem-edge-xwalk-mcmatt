1450a8c504b1877a74f466a7755c63eb487e3ade
Matt McKinley <mmckinley@merkleinc.com>
2024-09-30 09:48:33 -0600

    feature/tags - adding tags to test
    Testing tags display

---
+++ blocks/tags/tags.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+    const [tagsWrapper] = block.children;
+
+    const tagsContent = document.createElement('div');
+    tagsContent.classList.add('tags-content');
+
+    const metaTags = document.querySelectorAll('meta');
+  
+    for (const tag of metaTags) {
+      if (tag.getAttribute('name') == 'cq-tags') {
+        const content = tag.getAttribute('content');
+
+        tagsContent.textContent = content.trim();
+        tagsWrapper.replaceChildren(tagsContent);
+      }
+    }
+}
